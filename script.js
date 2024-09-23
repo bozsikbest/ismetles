881106fbd7de44eaefd560199394008838f18608
asd <asd>
2024-09-23 10:17:57 +0200

    asd

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
-var hell = false
 
-window.addEventListener("keypress", function(evt){
-    window.alert(evt.key)
-}) 
+var cellak = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
 
 function hello(){
-    document.getElementById("lehetHello").innerText = "Hello"
+    // while(true){
+    //     id = Math.floor(Math.random()*cellak.length)
+
+    //     if(cellak[id]!="asd"){
+    //         console.log(id)
+    //         document.getElementById(cellak[id]).innerText = "Hello"
+    //         console.log(cellak[id])
+    //         console.log(cellak)
+    //         cellak[id]="asd"
+    //         console.log(cellak)
+    //         break
+    //     }
     
-    document.getElementById("lehetHello").addEventListener("click", function(event){
+    // }
+    
+        
+    if (cellak.length == 0) {
+        cellak = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        for (elem of document.querySelectorAll("#table td")) {
+            elem.innerText="asd"
+        }
+        //return;
+    }
+        tombID = Math.floor(Math.random()*cellak.length)
+        elemID = cellak[tombID]
+        cellak.splice(tombID,1)
+
+        document.querySelector("#c"+elemID).innerText="HELLO"
+        console.log(elemID)
+        console.log(cellak)
+        
+    
+    }
+
+    
+    /*document.getElementById("lehetHello").addEventListener("click", function(event){
    
     document.getElementById("table").classList.add("arnyek")
 
-})
-}
+})*/
+
 
 
